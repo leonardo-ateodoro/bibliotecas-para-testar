666e39a044d2ad05cf5112ea3355d7d0da2fc77b
leonardo <leonardo.a.teodoro@aluno.senai.br>
2026-04-24 15:03:24 -0300

    terminando js

--- spa-fetch-components/js/main.js
+++ spa-fetch-components/js/main.js
@@ -4,7 +4,7 @@ import { iniciarMenu } from '../components/menu.js';
  */
 const carregarComponente = async (caminho, container) => {
     try {
-        const resposta = await fetch (caminho); 
+        const resposta = await fetch(caminho); 
         if (!resposta.ok) {
             throw new Error ('Erro ao carregar componente');
         }
@@ -14,8 +14,8 @@ const carregarComponente = async (caminho, container) => {
 
     /** Injeta no DOM */
     container.innerHTML = html;
-    } catch (erro)  {
-        console.error (erro); 
+}   catch (erro)  {
+    console.error (erro); 
 
     } 
 };
@@ -23,31 +23,50 @@ const carregarComponente = async (caminho, container) => {
  
 /**
  * Controla navegação SPA
- */
+ */ 
 const iniciarLinks = () => {
     const conteudo = document.querySelector('[data-conteudo]');
     document.querySelectorAll('[data-link]').forEach((link) => {
-        link.addEventListener('click, async (r') 
+        link.addEventListener('click', async (event) => {
+            event.preventDefault();
+            const pagina = link.dataset.link; 
 /** FECHA O MENU AO CLICAR */
-     
+            const menu = document.querySelector('[data-menu'); 
+            const botao = menu.querySelector ('.menu__botao');
 
+            if (menu.classList.contains('active')) {
+            
+                menu.classList.remove('active');
+                
+                botao.setAttribute('aria-expanded', false);
+                botao.setAttribute('aria-label', 'Abrir Menu');
+
+        }
  /** Carrega nova página */
-    
-
+            await carregarComponente(`./pages/${pagina}.html`, conteudo);
+        });
+    });
+};
 
 /**
  * Inicialização geral da aplicação
  */
-
-  /** Injetar menu */
-
-
-  /** Ativar comportamento do menu */
+    const iniciarAPP = async () => {
+        const containerMenu  = document.querySelector('[data-componente= "menu"]');
 
 
-  /** Ativar navegação SPA */
+    /** Injetar menu */
+        await carregarComponente('./components/menu.html', containerMenu);
 
 
-  /** Carrega página inicial */
+        /** Ativar comportamento do menu */
+        iniciarMenu();
 
+        /** Ativar navegação SPA */
+        iniciarLinks();
 
+        /** Carrega página inicial */
+        const conteudo = document.querySelector('[data-conteudo]');
+        await carregarComponente('./pages/home.html', conteudo);
+        };
+    iniciarAPP();
