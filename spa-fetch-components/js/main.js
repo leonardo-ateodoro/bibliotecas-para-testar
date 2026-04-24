4daf99257b93d27a79dce25d9db68f29f856936f
leonardo <leonardo.a.teodoro@aluno.senai.br>
2026-04-24 08:53:21 -0300

    continuando o js

--- spa-fetch-components/js/main.js
+++ spa-fetch-components/js/main.js
@@ -1,20 +1,33 @@
-
+import { iniciarMenu } from '../components/menu.js';
 /**
  * Carrega HTML via fetch e injeta no container
  */
-
+const carregarComponente = async (caminho, container) => {
+    try {
+        const resposta = await fetch (caminho); 
+        if (!resposta.ok) {
+            throw new Error ('Erro ao carregar componente');
+        }
 
     /** Converte para texto */
-    
+    const html = await resposta.text();
 
     /** Injeta no DOM */
-  
+    container.innerHTML = html;
+    } catch (erro)  {
+        console.error (erro); 
+
+    } 
+};
 
  
 /**
  * Controla navegação SPA
  */
-
+const iniciarLinks = () => {
+    const conteudo = document.querySelector('[data-conteudo]');
+    document.querySelectorAll('[data-link]').forEach((link) => {
+        link.addEventListener('click, async (r') 
 /** FECHA O MENU AO CLICAR */
      
 
