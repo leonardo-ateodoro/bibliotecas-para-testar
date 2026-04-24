e1c37b7d10909adfe1654b0f7d02f8f9216693a1
leonardo <leonardo.a.teodoro@aluno.senai.br>
2026-04-24 07:58:08 -0300

    iniciando o js

---
+++ spa-fetch-components/components/menu.js
@@ -0,0 +1,21 @@
+/**
+ * Inicializa comportamento do menu mobile
+ */
+
+
+    /**
+     * Alterna estado do menu
+     */
+  
+    /**
+     * Fecha o menu ao pressionar ESC
+     */
+   
+
+    /** Evento de clique */
+ 
+
+    /** Evento global de teclado */
+  
+  });
+};
---
+++ spa-fetch-components/js/main.js
@@ -0,0 +1,40 @@
+
+/**
+ * Carrega HTML via fetch e injeta no container
+ */
+
+
+    /** Converte para texto */
+    
+
+    /** Injeta no DOM */
+  
+
+ 
+/**
+ * Controla navegação SPA
+ */
+
+/** FECHA O MENU AO CLICAR */
+     
+
+ /** Carrega nova página */
+    
+
+
+/**
+ * Inicialização geral da aplicação
+ */
+
+  /** Injetar menu */
+
+
+  /** Ativar comportamento do menu */
+
+
+  /** Ativar navegação SPA */
+
+
+  /** Carrega página inicial */
+
+
