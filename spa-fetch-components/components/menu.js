46ea35ba78fc0c7b0f9809f40305a36785acc5b0
Leonardo <leonardo.a.teodoro@aluno.senai.br>
2026-05-07 09:01:42 -0300

    js

--- spa-fetch-components/components/menu.js
+++ spa-fetch-components/components/menu.js
@@ -1,19 +1,37 @@
 /**
  * Inicializa comportamento do menu mobile
  */
-
-
+export const iniciarMenu = () => {
+  const menus = document.querySelectorAll ('[data-menu]');
+  menus.forEach((menu) => {
+    const botao = menu.querySelector('.menu__botao');
+    
     /**
      * Alterna estado do menu
      */
-  
+    const toggleMenu = () => {
+      menu.classList.toggle('active');
+      const ativo = menu.classList.contains('active');
+      botao.setAttribute('aria-expanded',ativo); 
+      botao.setAttribute('aria-label', ativo ? 'Fechar Menu' : 'Abrir Menu');
+    };
     /**
      * Fecha o menu ao pressionar ESC
      */
+    const fecharComEsc = (event) => {
+      const isEsc = event.key === 'Escape';
+      const ativo = menu.classList.contains('active');
+      if (isEsc && ativo) {
+        menu.classList.remove('active');
+        botao.setAttribute('aria-expanded', false);
+        botao.setAttribute('aria-label', 'Abrir Menu');
+        botao.focus();
+      }
+       };
    
 
     /** Evento de clique */
- 
+    botao.addEventListener
 
     /** Evento global de teclado */
   
